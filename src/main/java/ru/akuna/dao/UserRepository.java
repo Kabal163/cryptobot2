33dcab27c08cc67b4c964486b6c3435aebbc1c97
Affected <affected700@yandex.ru>
2018-01-21 22:04:50 +0400

    1. Removed demo classes 2. Removed extra imports

--- src/main/java/ru/akuna/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.akuna.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<BittrexMarket, Long>
-{
-
-}
--- src/main/java/ru/akuna/dao/listeners/BittrexMarketListener.java
+++ src/main/java/ru/akuna/dao/listeners/BittrexMarketListener.java
@@ -1,14 +1,9 @@
 package ru.akuna.dao.listeners;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import ru.akuna.dao.BittrexMarket;
-import ru.akuna.providers.ApplicationContextProvider;
-import ru.akuna.strategy.job.TickerJob;
-import ru.akuna.tools.job.JobScheduler;
 import ru.akuna.tools.job.TickerJobScheduler;
-import ru.akuna.tools.properties.ApplicationProperties;
 
 import javax.persistence.PostPersist;
 import javax.persistence.PostRemove;
